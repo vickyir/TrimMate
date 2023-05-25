c5ed20cf826c6c1a0ea5e83e838e29afc1d114c1
VickyIr <vicky.ir@students.amikom.ac.id>
2023-05-26 01:12:20 +0700

    Initial Commit

---
+++ AssistantReality/AssistantRealityApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssistantRealityApp.swift
+//  AssistantReality
+//
+//  Created by Vicky Irwanto on 26/05/23.
+//
+
+import SwiftUI
+
+@main
+struct AssistantRealityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
